80e4cd3c77c17c120b144fff5c87b775ec2c6487
Tom Dyer <tdyer1@gmail.com>
2010-03-20 22:53:09 -0400

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  
+  integrate_views
+  
   #Delete these examples and add some real ones
   it "should use PagesController" do
     controller.should be_an_instance_of(PagesController)
@@ -19,6 +21,22 @@ describe PagesController do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
+  %w{ Home Contact About}.each do |title|
+
+    it "should have the right title" do
+      get "#{title.downcase}"
+      response.should have_tag('title',
+                               "Ruby on Rails Tutorial Sample App | #{title}")
     end
   end
 end
